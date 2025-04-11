6c05bb5d8187fc9686b33f4c084fb9cf5847d592
Muhammed Aslam <muhammedbinramli@gmail.com>
2025-04-11 23:23:02 +0530

    footer component created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,20 @@
+import Footer from "./Footer.jsx";
 function App() {
- return (
-  <header className="row justify-between"id="one">
-      <div>
-          Logo
-      </div>
-      <div>
+  return (
+    <>
+      <header className="row justify-between" id="one">
+        <div>Logo</div>
+        <div>
           <ul className="flex gap-[25vw]">
-            <li>
-              cart
-            </li>
-            <li>
-              wishlist
-            </li>
-            <li>
-              profile
-            </li>
+            <li>cart</li>
+            <li>wishlist</li>
+            <li>profile</li>
           </ul>
-      </div>
-    </header>
- )
+        </div>
+      </header>
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
