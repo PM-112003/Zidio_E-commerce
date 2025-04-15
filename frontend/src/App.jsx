d2af9401ab1e850540a6ab1e42179e86dfc30522
Muhammed Aslam <muhammedbinramli@gmail.com>
2025-04-15 19:54:48 +0530

    readme file added in backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,16 @@
 function App() {
- return (
-  <header className="row justify-between"id="one">
+  return (
+    <header className="row justify-between" id="one">
+      <div>Logo</div>
       <div>
-          Logo
-      </div>
-      <div>
-          <ul className="flex gap-[25vw]">
-            <li>
-              cart
-            </li>
-            <li>
-              wishlist
-            </li>
-            <li>
-              profile
-            </li>
-          </ul>
+        <ul className="flex gap-[25vw]">
+          <li>cart</li>
+          <li>wishlist</li>
+          <li>profile</li>
+        </ul>
       </div>
     </header>
- )
+  );
 }
 
-export default App
+export default App;
